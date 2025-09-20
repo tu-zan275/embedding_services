c47f838f1c83af7e852694f494b58605723d5f1b
SAI-IT\tu.vancong <tu.vancong@sai-digital.com>
2025-09-21 00:42:16 +0700

    up

---
+++ setup_package.sh
@@ -0,0 +1,11 @@
+# Cài virtualenv nếu chưa có
+pip3 install virtualenv
+
+# Tạo môi trường ảo
+python3 -m venv venv
+
+# Kích hoạt
+source venv/bin/activate
+
+# Cài package
+pip install -r requirements.txt
